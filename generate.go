4863dabb230e487357e553e9e2fa9318c07562ad
circa10a <caleblemoine@gmail.com>
2026-02-09 15:47:18 -0800

    #patch: switch go generate to use go tool

--- generate.go
+++ generate.go
@@ -1,2 +1,2 @@
-//go:generate go run -modfile=go.mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=api/oapi-codegen.yaml api/openapi.yaml
+//go:generate go tool oapi-codegen --config=api/oapi-codegen.yaml api/openapi.yaml
 package main
